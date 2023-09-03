d3a8f212dd267fc00c0f38429651ab9839fac5ac
OertMahmutaj <oert1994@icloud.com>
2023-09-03 10:43:21 +0200

    first commit

---
+++ src/components/TodoApp.jsx
@@ -0,0 +1,78 @@
+import React, { useState, useEffect } from 'react';
+
+function TodoApp() {
+  const [tasks, setTasks] = useState([]);
+  const [newTask, setNewTask] = useState('');
+
+  useEffect(() => {
+    const savedTasks = localStorage.getItem('tasks');
+    if (savedTasks) {
+      setTasks(JSON.parse(savedTasks));
+    }
+  }, []);
+ 
+  useEffect(() => {
+    localStorage.setItem('tasks', JSON.stringify(tasks));
+  }, [tasks]);
+
+  const handleInputChange = (e) => {
+    setNewTask(e.target.value);
+  };
+
+  const addTask = () => {
+    if (newTask.trim() !== '') {
+      setTasks([...tasks, { text: newTask, completed: false }]);
+      setNewTask('');
+    }
+  };
+
+  const removeTask = (index) => {
+    const updatedTasks = tasks.filter((_, i) => i !== index);
+    setTasks(updatedTasks);
+  };
+
+  const toggleTaskCompletion = (index) => {
+    const updatedTasks = [...tasks];
+    updatedTasks[index].completed = !updatedTasks[index].completed;
+    setTasks(updatedTasks);
+  };
+
+  return (
+    <div>
+      <h1>Todo List</h1>
+      <div>
+        <input
+          type="text"
+          placeholder="Add a new task"
+          value={newTask}
+          onChange={handleInputChange}
+        />
+        <button onClick={addTask}>Add</button>
+      </div>
+      <ul>
+        {tasks.map((task, index) => (
+          <li key={index}>
+            <input
+              type="checkbox"
+              checked={task.completed}
+              onChange={() => toggleTaskCompletion(index)}
+            />
+            <span className={task.completed ? 'completed' : ''}>
+              {task.text}
+            </span>
+            <button onClick={() => removeTask(index)}>Delete</button>
+          </li>
+        ))}
+      </ul>
+      <style>
+        {`
+          .completed {
+            text-decoration: line-through;
+          }
+        `}
+      </style>
+    </div>
+  );
+}
+
+export default TodoApp;
